283a4d3447176edda208964806ad6460844eaca2
Jonatan <jonatan@jontes.page>
2023-02-05 18:21:59 +0100

    feat: Add terms and fix downloads

--- src/components/Downloads.svelte
+++ src/components/Downloads.svelte
@@ -3,11 +3,14 @@
   let downloads = 0;
 
   async function fetchData() {
-    const response = await fetch("https://api.floopr.org/v1/publicstats");
+    const response = await fetch(
+      "https://stats.jontes.page/api/stats/floopr.org/conversions?period=all&filters=%7B%7D&with_imported=true"
+    );
     const json = await response.json();
-    downloads = json.downloads;
+    downloads = json[0].total_conversions;
   }
 
   onMount(fetchData);
 </script>
+
 {downloads}
